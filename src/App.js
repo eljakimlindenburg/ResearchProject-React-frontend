55a278a335a76fb6002d4fafedd27759aac2c1a3
Eljakim Thuis <eljakim.l@gmail.com>
2020-05-27 12:28:58 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import MainLayout from "./shared/mainlayout/mainlayout.component";
+import {CustomHeader} from "./shared/customheader/customheader.component";
+import {Route, Switch} from "react-router-dom";
+import IndexPage from "./pages/indexpage/indexpage.component";
+import ShoppingCart from "./pages/shoppingcart/shoppingcart.component";
+import DetailPage from "./pages/detailpage/detailpage.component";
 
 class App extends React.Component {
     render() {
         return (
-            <MainLayout/>
+            <div>
+                <div className="container-fullwidth bg-dark">
+                    <CustomHeader />
+                </div>
+                <main className="main">
+                    <Switch>
+                        <Route exact path='/' component={IndexPage}/>
+                        <Route exact path='/shoppingcart' component={ShoppingCart} />
+                        <Route exact path='/detail' component={DetailPage}/>
+                        <Route path="/detail/:id" component={DetailPage} />
+                    </Switch>
+                </main>
+            </div>
         );
     }
 }
